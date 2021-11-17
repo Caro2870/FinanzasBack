670168f81eb0c2abc6466e272ec1b811c63143f8
Caro2870 <55043767+Caro2870@users.noreply.github.com>
2021-11-17 00:14:45 -0500

    Update ReasonController.java

--- src/main/java/com/finanzas/finanzasback/controller/ReasonController.java
+++ src/main/java/com/finanzas/finanzasback/controller/ReasonController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ReasonController {
     @Autowired
